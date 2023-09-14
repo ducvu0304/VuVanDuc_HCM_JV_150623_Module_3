be6e94c38136cb38ca59d0aa29597f18f9e895fd
Đức Vũ <ducvu2509@gmail.com>
2023-09-14 09:54:22 +0700

    Session1 / 11-09-23

---
+++ src/rikkei/academy/Timer.java
@@ -0,0 +1,11 @@
+package rikkei.academy;
+
+import java.util.Date;
+
+public class Timer {
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        System.out.println("Time now: " + date);
+    }
+}
