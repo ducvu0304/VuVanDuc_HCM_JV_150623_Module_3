5818bfcf61e4849588477807ff9b996c056f829a
Đức Vũ <ducvu2509@gmail.com>
2023-09-17 20:18:43 +0700

    Nộp bài session5 17-09-2023

--- src/exercise/EmailExampleTest.java
+++ src/exercise/EmailExampleTest.java
@@ -2,16 +2,18 @@ package exercise;
 
 public class EmailExampleTest {
     private static EmailExample emailExample;
-    public static final String[] validEmail = new String[] { "a@gmail.com", "ab@yahoo.com", "abc@hotmail.com" };
-    public static final String[] invalidEmail = new String[] { "@gmail.com", "ab@gmail.", "@#abc@gmail.com" };
+    public static final String[] validEmail = {"a@gmail.com", "ab@yahoo.com", "abc@hotmail.com"};
+    public static final String[] invalidEmail ={ "@gmail.com", "ab@gmail.", "@#abc@gmail.com" };
 
 
     public static void main(String args[]) {
         emailExample = new EmailExample();
+
         for (String email : validEmail) {
-            boolean isvalid = emailExample.validate(email);
-            System.out.println("Email is " + email +" is valid: "+ isvalid);
+            boolean isValid = emailExample.validate(email);
+            System.out.println("Email is " + email +" is valid: "+ isValid);
         }
+
         for (String email : invalidEmail) {
             boolean isvalid = emailExample.validate(email);
             System.out.println("Email is " + email +" is valid: "+ isvalid);
