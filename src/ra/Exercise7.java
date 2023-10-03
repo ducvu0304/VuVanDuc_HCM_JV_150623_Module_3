fc3e1346bf1a80e8516431217fdafdca5a014c17
Đức Vũ <ducvu2509@gmail.com>
2023-10-03 21:27:53 +0700

    nộp bài session12 / 03-10

---
+++ src/ra/Exercise7.java
@@ -0,0 +1,24 @@
+package ra;
+
+import java.util.*;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        Integer[] arr = {2,22,32,3,3,4,4,65,2,5};
+        Map<Integer, Integer> treeMap = new TreeMap<>(Comparator.comparingInt(a -> (int) a).reversed());
+
+        for (int i = 0; i < arr.length; i++) {
+            treeMap.put(arr[i], 0);
+        }
+
+        int i = 0;
+        for (Map.Entry<Integer,Integer> m: treeMap.entrySet()) {
+            if(i == 1) {
+                System.out.println(m.getKey());
+                break;
+            }
+            i++;
+        }
+
+    }
+}
---
+++ src/ra/Exercise8.java
@@ -0,0 +1,24 @@
+package ra;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        Integer[] arr = {2,22,32,3,3,4,4,65,2,5};
+        Map<Integer, Integer> treeMap = new TreeMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            treeMap.put(arr[i], 0);
+        }
+
+        for (Map.Entry<Integer,Integer> m: treeMap.entrySet()) {
+            int i = 1;
+            if(i == 1) {
+                System.out.println(m.getKey());
+                break;
+            }
+        }
+    }
+}
